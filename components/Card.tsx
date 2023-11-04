4d3dbec67ab46c499ff621b6f964ae2a96083ebd
branded10 <branded_xyz_1232@outlook.com>
2023-11-04 13:13:03 +0530

    updated components files

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,7 +3,9 @@
 import Image from "next/image";
 import  {Swiper, SwiperSlide} from 'swiper/react';
 import  { Navigation, Pagination, Scrollbar, A11y, Mousewheel } from 'swiper/modules';
+
 import 'swiper/css';
+import 'swiper/css/free-mode';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
@@ -52,24 +54,24 @@ const Card = ({
   return (
     <div>
       <div
-        className={`w-[450px] ${
-          isFifth ? "h-[414px] mt-10" : "h-[512px]"
-        } bg-white rounded-[30px] border-[2px] border-b-[5px] ${
+        className={`w-[320px] ${
+          isFifth ? "h-[305px] mt-10" : "h-[372px]"
+        } ${isThird ? "h-[372px]" : ""} bg-white rounded-[30px] border-[2px] border-b-[5px] ${
           isOpaque && "opacity-50"
         }`}
         style={{ borderColor: borderColor }}
       >
         <div
-          className={`flex justify-between items-center px-6 py-3 ${
+          className={`flex justify-between items-center px-4 py-2 ${
             isFourth && "bg-[#7645D9]"
           } ${isFifth && "bg-[#E7E3EB]"} rounded-t-[30px] overflow-hidden `}
         >
           <div className="flex items-center">
             <div>
-              <Image src={icon} width={30} height={30} alt="play" />
+              <Image src={icon} width={20} height={20} alt="play" />
             </div>
             <div
-              className={`ml-1 font-extrabold text-xl`}
+              className={`ml-1 font-extrabold text-sm`}
               style={{ color: txtColor }}
             >
               {liveText}
@@ -88,13 +90,13 @@ const Card = ({
           </div>
         )}
 
-        <div className={`flex flex-col justify-center items-center mt-6`}>
+        <div className={`flex flex-col justify-center items-center mt-4`}>
           {!isFifth && (
             <div className={`-mb-[2px]`}>
               <Image
                 src="card_up_white.svg"
-                width={350}
-                height={580}
+                width={250}
+                height={380}
                 alt="image"
               />
             </div>
@@ -103,40 +105,42 @@ const Card = ({
             <div className={`-mb-[2px]`}>
               <Image
                 src="card_up_last.svg"
-                width={350}
-                height={580}
+                width={250}
+                height={380}
                 alt="image"
               />
             </div>
           )}
+
+          {/* middle part of the card */}
           <div
-            className={`flex flex-col p-4 rounded-3xl border-[3.5px] w-[400px] ${
-              isFifth ? "h-[113px]" : "h-[215px]"
-            } ${isFourth && "h-[225px]"} bg-white`}
+            className={`flex flex-col p-4 rounded-2xl border-[2.5px] w-[285px] ${
+              isFifth ? "h-[95px]" : "h-[155px]"
+            } ${isFourth ? "h-[171px]" : ""} bg-white`}
             style={{ borderColor: innerBorderColor }}
           >
             {!isFourth && !isFifth && (
-              <div className={`font-bold text-[#7b6ba7] mb-[15px]`}>
+              <div className={`font-extrabold text-xs text-[#7b6ba7] mb-[8px]`}>
                 LAST PRICE
               </div>
             )}
             {!isFourth && !isFifth && (
               <div className={`flex justify-between items-center mb-6`}>
                 <div
-                  className={`font-bold text-3xl text-[#ef449c] underline_dotted`}
+                  className={`font-bold text-xl text-[#ef449c] underline_dotted`}
                 >
                   {lastPrice}
                 </div>
                 <div
-                  className={`flex justify-center items-center bg-[#ef449c] py-2 px-2 rounded-md gap-2`}
+                  className={`flex justify-center items-center bg-[#ef449c] py-[5px] px-2 rounded-md gap-2`}
                 >
                   <Image
                     src="card_down_arrow.svg"
-                    width={30}
-                    height={30}
+                    width={20}
+                    height={20}
                     alt="arrow"
                   />
-                  <p className={`text-white text-xl font-medium`}>
+                  <p className={`text-white text-sm font-medium`}>
                     {priceChange}
                   </p>
                 </div>
@@ -144,32 +148,32 @@ const Card = ({
             )}
             {!isFourth && !isFifth && (
               <div
-                className={`flex justify-between items-center font-semibold text-lg text-[#4e459b] mb-1`}
+                className={`flex justify-between items-center font-semibold text-xs text-[#280d5f] mb-1`}
               >
                 <div>Locked Price:</div>
                 <div>{lockedPrice}</div>
               </div>
             )}
             {!isFifth && (
-              <div className="flex justify-between items-center font-extrabold text-xl text-[#280D5F]">
+              <div className="flex justify-between items-center font-extrabold text-md text-[#280D5F]">
                 <div>Prize Pool:</div>
-                <div>8.5143BNB</div>
+                <div>8.5143 BNB</div>
               </div>
             )}
             {isFourth && (
               <div className="text-white font-extrabold flex flex-col justify-center items-center mt-4 gap-1">
-                <div className="bg-[#31D0AA] p-3 w-[300px] py-5 rounded-2xl shadow-sm shadow-black text-center text-xl">
+                <div className="bg-[#31D0AA] p-1 w-[250px] py-3 rounded-2xl shadow-sm shadow-black text-center text-md">
                   Enter UP
                 </div>
-                <div className="bg-[#ED4B9E] p-3 w-[300px] py-5 rounded-2xl shadow-sm shadow-black text-center text-xl">
+                <div className="bg-[#ED4B9E] p-1 w-[250px] py-3 rounded-2xl shadow-sm shadow-black text-center text-md">
                   Enter Down
                 </div>
               </div>
             )}
             {isFifth && (
               <div className="flex flex-col gap-3 text-[#280D5F] justify-center items-center">
-                <div className="text-xl font-bold">Entry starts</div>
-                <div className="text-3xl font-extrabold">~00:38</div>
+                <div className="text-md font-bold">Entry starts</div>
+                <div className="text-2xl font-extrabold">~00:38</div>
               </div>
             )}
           </div>
@@ -177,8 +181,8 @@ const Card = ({
             <div className={`-mt-[2px]`}>
               <Image
                 src="card_down_pink.svg"
-                width={350}
-                height={580}
+                width={250}
+                height={380}
                 alt="image"
               />
             </div>
@@ -188,8 +192,8 @@ const Card = ({
             <div className={`-mt-[2px]`}>
               <Image
                 src="card_down_last.svg"
-                width={350}
-                height={580}
+                width={250}
+                height={380}
                 alt="image"
               />
             </div>
@@ -268,13 +272,19 @@ export default function Cards() {
   };
 
   return (
+   
+
     <Swiper
     style={{ width: '100%', overflow: 'hidden' }}
       spaceBetween={209}
-      slidesPerView={3}
+      slidesPerView={4}
+      centeredSlides={true} 
+      centeredSlidesBounds={true}
+      scrollbar={false}
+      freeMode={true}
       // navigation
       pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
+      // scrollbar={{ draggable: true }}
       // mousewheel={{ forceToAxis: true }}
       mousewheel={true}
       breakpoints={{
@@ -289,7 +299,7 @@ export default function Cards() {
           slidesPerView: 2,
         },
       }}
-    >
+      >
     
 
       {cardsData.map((card, index) => {
@@ -300,7 +310,11 @@ export default function Cards() {
         const isFourthCard = index === 3;
         const isFifthCard = index === 4;
         return (
+          
+
           <SwiperSlide key={index}>
+          
+
             <Card
               key={index}
               {...card}
@@ -310,11 +324,14 @@ export default function Cards() {
               isFifth={isFifthCard}
               isThird={isThirdCard}
               />
+          
            </SwiperSlide>
+          
         );
       })}
       
      </Swiper>
+      
   );
 }
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,40 +4,40 @@ const navbar = () => {
   return (
     <div className="w-full -mt-52 flex justify-between items-center">
       <div className="relative flex items-center justify-center ml-14 ">
-        <div className="absolute -left-[68px] ">
+        <div className="absolute -left-[30px] ">
           <Image
             src="/binance_logo.png"
-            width={120}
-            height={120}
+            width={80}
+            height={80}
             alt="Binance image"
           />
         </div>
-        <div className="flex items-center justify-center bg-[#ffffff] px-5 py-3 pl-[40px] rounded-full">
-          <div className="mr-[10px] font-extrabold text-[#29005c] text-2xl">
+        <div className="flex items-center justify-center bg-[#ffffff] px-5 py-1 pl-[40px] rounded-full">
+          <div className="mr-[20px] font-extrabold text-[#29005c] text-xl">
             BNBUSD
           </div>
-          <div className="text-[#29005c] font-medium">$228.5332</div>
+          <div className="text-[#29005c] font-medium text-xs">$228.5332</div>
         </div>
       </div>
       <div className="hidden justify-between items-center px-[30px]  nav_cards:flex">
-        <div className="relative left-[137px]">
-          <Image src="nav_card.svg" width={140} height={140} alt="nav_cards" />
+        <div className="relative left-[107px]">
+          <Image src="nav_card.svg" width={100} height={100} alt="nav_cards" />
         </div>
         <div className="w-full">
           <div className="bg-white  rounded-full gap-14 flex justify-between items-center py-1 px-1">
             <button>
               <Image
                 src="nav_arrow_left.svg"
-                width={35}
-                height={35}
+                width={25}
+                height={25}
                 alt="arrow"
               />
             </button>
             <button>
               <Image
                 src="nav_arrow_right.svg"
-                width={35}
-                height={35}
+                width={25}
+                height={25}
                 alt="arrow"
               />
             </button>
@@ -47,16 +47,16 @@ const navbar = () => {
       <div className="w-[600px]">
         <div className="flex p-5 items-center">
           <div className="relative">
-            <div className="bg-white rounded-full flex justify-center items-center px-3 py-2 pr-[65px] gap-2 mr-10">
-              <div className="text-3xl text-[#7836d3] font-bold">00:38</div>
-              <div className="font-medium">5m</div>
+            <div className="bg-white rounded-full flex justify-center items-center px-3 py-2 pr-[65px] gap-2 mr-4">
+              <div className="text-xl text-[#7836d3] font-extrabold">00:38</div>
+              <div className="font-medium text-xs">5m</div>
             </div>
             {/* right-7 top-[341px] */}
-            <div className="absolute  right-[10px] -top-[21px]">
-              <Image src="clock.svg" width={90} height={90} alt="clock" />
+            <div className="absolute  right-[8px] -top-[21px]">
+              <Image src="clock.svg" width={70} height={70} alt="clock" />
             </div>
           </div>
-          <div className="flex gap-[7px] mr-[50px] items-center justify-center">
+          <div className="flex gap-[7px] mr-[1px] items-center justify-center">
             <div className="bg-[#7b6ba7] rounded-2xl w-[50px] h-[50px] flex items-center justify-center"> 
               <Image src="question.svg" width={24} height={24} alt="ques" className="py-3 items-center justify-center" />
             </div>
