ece5fab4ef31a4accda06b13b43c1fb42968e3e4
branded10 <branded_xyz_1232@outlook.com>
2023-11-17 16:06:19 +0530

    updated

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { signIn } from "next-auth/react";
+import { Quicksand } from "next/font/google";
 import Image from "next/image"; // we need to config it in next.config.js,,, se Note.txt
 
 import React from "react";
@@ -7,7 +8,9 @@ import React from "react";
 // bg-[#11a37f]
 const Login = () => {
   return (
-    <div className="bg-[#11364b] h-screen flex flex-col items-center justify-center text-center login_font">
+    <div
+      className={`bg-[#11364b] h-screen flex flex-col items-center justify-center text-center login_font`}
+    >
       <Image
         src="/rupee_img.png"
         width={500}
@@ -18,7 +21,7 @@ const Login = () => {
       <button className="text-white font-bold text-3xl -mt-10 login_font">
         <span
           onClick={() => signIn("google")}
-          className="bg-white text-green-500 rounded-2xl border-b-8 p-4 animate-pulse login_font"
+          className="bg-white text-green-500 rounded-2xl border-b-8 p-4 animate-pulse login_font mr-3"
         >
           Sign In
         </span>{" "}
